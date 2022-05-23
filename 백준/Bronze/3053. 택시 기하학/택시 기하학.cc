60c404e7e8ae02f68ca897c750ad8041a92d4c39
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-05-23 21:11:51 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+
+int main() {
+
+    double N ;
+    cin >> N;
+
+    cout<<fixed;
+    cout.precision(5);
+
+
+    cout << M_PI * pow(N,2) << "\n";
+    cout << (double)2 * pow(N,2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
