d32f0cd333a2b5b598b1cbf2f0079bd43e225b2e
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-06-02 22:12:32 +0900

    [Bronze I] Title: 이항 계수 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, k;
+    cin >> n >> k;
+    int N[11] = { 1, };
+    int K[11] = { 1, };
+    for (int i = n-k+1; i <= n; i++) {
+        N[0] *= i;
+    }
+    for (int i = 1; i <= k; i++) {
+        K[0] *= i;
+    }
+    cout << (N[0] / K[0]);
+
+}
