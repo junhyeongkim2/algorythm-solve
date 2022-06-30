4f77a0c38d51549892f2b60258716fd44b3fc137
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-07-01 00:15:16 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+
+int N;
+
+using namespace std;
+
+int main(){
+    cin >> N;
+
+    int c1=0;
+    int c2=0;
+
+    c1 = N-1;
+    c2=1;
+
+    for(int i = 0 ; i< N-1;i++){
+        for(int i1=0;i1<c1;i1++){
+            cout << " ";
+        }
+        for(int i2=0;i2<c2;i2++){
+            cout <<  "*";
+        }
+        c1--;
+        c2++;
+        cout << "\n";
+    }
+
+    for(int i=0;i<c2;i++){
+        cout <<"*";
+    }
+    cout << "\n";
+
+    c2-=1;
+    c1=1;
+
+    for(int i = 0 ; i< N-1;i++){
+        for(int i1=0;i1<c1;i1++){
+            cout << " ";
+        }
+        for(int i2=0;i2<c2;i2++){
+            cout <<  "*";
+        }
+        c1++;
+        c2-=1;
+        cout << "\n";
+    }
+
+
+
+
+
+
+
+
+
+}
