2674d1742e28f75cd5ea9eb2a931eb789a7cc1f9
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-07-01 22:20:28 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 1144 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -2,63 +2,67 @@
 #include <string.h>
 
 using namespace std;
-
-int n, cnt = 0;
+int N=0;
 char photo[100][100];
-bool isVisited[100][100] = { false };
+bool isVisited[100][100] = {false};
+int cnt=0;
 
-int dx[4] = { 0,0,-1,1 };
-int dy[4] = { -1,1,0,0 };
+int dx[4] = {0,0,-1,1};
+int dy[4] = {-1,1,0,0};
 
-void dfs(int x, int y) {
-    isVisited[x][y] = true;
-    for (int i = 0; i < 4; i++) {
-        int nx = x + dx[i];
+
+void dfs(int x , int y) {
+    isVisited[x][y]=true;
+    for(int i = 0 ; i < 4; i++) {
+        int nx = x  + dx[i];
         int ny = y + dy[i];
 
-        if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-        if (!isVisited[nx][ny] && photo[x][y] == photo[nx][ny]) {
-            dfs(nx, ny);
+        if(nx<0||nx>=N||ny<0||ny>=N)continue;
+        if(!isVisited[nx][ny]&&photo[x][y]==photo[nx][ny]) {
+            dfs(nx,ny);
         }
     }
 }
 
 int main() {
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++) {
         scanf("%s", photo[i]);
     }
 
-    //적록색약이 아닌 사람
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (!isVisited[i][j]) {
-                dfs( i, j);
-                cnt++;
-            }
-        }
-    }
-    printf("%d ", cnt);
 
-    //적록색약인 사람
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (photo[i][j] == 'G') photo[i][j] = 'R';
-            else photo[i][j] = photo[i][j];
-        }
-    }
-    memset(isVisited, false, sizeof(isVisited));
-
-    cnt = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             if (!isVisited[i][j]) {
                 dfs(i, j);
                 cnt++;
             }
         }
     }
-    printf("%d", cnt);
+
+    printf("%d ",cnt);
+
+    for(int i=0;i<N;i++){
+        for(int j = 0 ; j < N;j++){
+            if(photo[i][j]=='G')photo[i][j]='R';
+            else photo[i][j] = photo[i][j];
+        }
+    }
+    
+    memset(isVisited,false,sizeof(isVisited));
+
+    cnt=0;
+
+    for(int i = 0 ; i < N;i++){
+        for(int j = 0 ; j < N;j++){
+            if(!isVisited[i][j]){
+                dfs(i,j);
+                cnt++;
+            }
+        }
+    }
+
+    printf("%d",cnt);
 
     return 0;
 }
