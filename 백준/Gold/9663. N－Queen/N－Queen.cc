68c20adb047b62af3efa2032192e36e39932a2e4
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-07-04 17:09:02 +0900

    [Gold IV] Title: N-Queen, Time: 4248 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -2,45 +2,34 @@
 #define MAX 15
 using namespace std;
 
-int N,total=0;
 int col[MAX];
+int N, total = 0;
 
-
-bool check(int level){
-    for(int i = 0; i < level;i++){
-        if(col[i]==col[level]||abs(col[level]-col[i])==level-i){
+bool check(int level)
+{
+    for(int i = 0; i < level; i++)
+        if(col[i] == col[level] || abs(col[level] - col[i]) == level - i)
             return false;
-        }
-    }
+     
     return true;
 }
-void nqueen(int x){
 
-    if(x==N){
+void nqueen(int x)
+{
+    if(x == N)
         total++;
-    }else{
-        for(int i = 0 ; i < N ; i++ ){
+    else
+    {
+        for(int i = 0; i < N; i++)
+        {
             col[x] = i;
-            if(check(x)){
+            if(check(x)) 
                 nqueen(x+1);
-            }
         }
     }
-
 }
-
-
-
-
-
-
-
-int main(){
+int main() {
     cin >> N;
     nqueen(0);
     cout << total;
-
 }
-
-
-
