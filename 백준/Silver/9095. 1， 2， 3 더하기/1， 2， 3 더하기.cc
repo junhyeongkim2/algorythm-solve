3e900636ac73496899fbdf5f3bc5d3c8ba8e23be
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-07-17 02:18:43 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int T;
+int n;
+
+int dp[11];
+
+int main(){
+
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+    for(int i = 4; i<=11;i++){
+        dp[i] = (dp[i-1] + dp[i-2] + dp[i-3]);
+    }
+    cin >> T;
+    while(T--){
+        cin >> n;
+        cout << dp[n] << "\n";
+
+    }
+
+
+
+
+
+}
