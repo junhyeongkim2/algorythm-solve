7976d8b76cfbd278703ecbab23a9b1f7fc57e240
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-07-23 21:58:08 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -8,15 +8,15 @@ int main(){
     string st1,st2;
     cin >> st1>> st2;
 
-    for(int i= 1; i <=st2.size();i++){
-        for(int j=1;j<=st1.size();j++){
-            if(st2[i-1] == st1[j-1])
+    for(int i= 1; i <=st1.size();i++){
+        for(int j=1;j<=st2.size();j++){
+            if(st1[i-1] == st2[j-1])
                 dp[i][j] = dp[i-1][j-1] + 1;
             else
                 dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
         }
     }
-    cout << dp[st2.size()][st1.size()] << "\n";
+    cout << dp[st1.size()][st2.size()] << "\n";
 
 
 
