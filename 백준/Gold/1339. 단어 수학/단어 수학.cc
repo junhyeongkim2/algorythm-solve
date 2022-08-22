b0ccbbeaedf22393b40cdb2abfb6e33b90607e57
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-08-22 21:28:01 +0900

    [Gold IV] Title: 단어 수학, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.cc
+++ 백준/Gold/1339. 단어 수학/단어 수학.cc
@@ -1,48 +1,37 @@
 #include <iostream>
 #include <algorithm>
-#include <string>
 
 using namespace std;
 
 int graph[26];
-bool comp(int &a, int &b){
-    return a>b;
-}
-
+int N;
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    
-    int n ;
-    cin >> n;
-    
-    for(int i = 0 ; i < n ;i++){
+    cin >> N;
+    for(int i= 0 ; i < N ; i++){
         string s;
         cin >> s;
-        int pow =1;
+        int pow=1;
         for(int i1=s.length()-1;i1>=0;i1--){
-            graph[s[i1]-'A']+=pow;
-            pow*=10;
+            graph[s[i1]-'A'] += pow;
+            pow*=10; 
         }
     }
     
-    sort(graph,graph+26,comp);
-    
+    sort(graph,graph+26,greater<>());
     int answer=0;
-    int count=9; 
-  
-    for(int i =0; i < 26;i++){
-        if(graph[i]==0){
+    int cnt=9;
+    
+    for(int i = 0 ; i < 26;i++){
+        if(graph[i]==0)
             break;
-        }
-        answer += graph[i] * count--;
+        answer += graph[i] * cnt--;
     }
-    
     cout << answer << "\n";
-    return 0;
     
     
+    
+    
+   
     
     
 }
