ce4d4e773f915f16d9c4035ebcae3b2f5ababf92
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-08-24 16:49:09 +0900

    [Silver III] Title: N과 M (8), Time: 8 ms, Memory: 2064 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int M,N;
+
+int arr[9];
+int visited[10000];
+vector<int>graph;
+
+
+
+
+void back(int sx, int len){
+
+    if(len > N){
+        for (int i = 1; i <=N ; ++i) {
+            cout << arr[i]<<" ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for(int i = sx ; i <M;i++){
+        //cout << graph[i] << "\n";
+        if(visited[graph[i]]==0){
+            arr[len] = graph[i];
+            back(i,len+1);
+        }
+    }
+
+
+}
+
+
+
+int main(){
+
+    cin >> M >> N;
+
+    for (int i = 0; i <M ; ++i) {
+        int a ;
+        cin >> a;
+        graph.push_back(a);
+    }
+
+
+    std::sort(graph.begin(), graph.end());
+
+
+    back(0,1);
+
+
+
+
+}
