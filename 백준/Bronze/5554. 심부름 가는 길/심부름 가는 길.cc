4c5ce5637fe101f6093600c2187a2a70963cfbbc
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-09-17 01:27:50 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a , b, c, d;
+
+int main(){
+
+    cin >> a >> b >> c>> d;
+
+    int sum = 0;
+
+    sum = a+b+c+d;
+
+    int e = sum / 60;
+    int f = sum % 60;
+
+    cout << e << "\n" << f;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
