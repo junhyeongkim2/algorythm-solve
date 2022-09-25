2b3c40d5f4fcfb02be373b78c5bcb5a56d9f62fc
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-09-26 03:34:34 +0900

    [Bronze IV] Title: 과자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int a, b, c ;
+
+int main(){
+
+    cin >> a >> b >> c;
+
+
+    if( (a*b) > c  ){
+        cout << ((a*b) -c);
+    }else{
+        cout << 0;
+    }
+
+
+
+
+
+
+
+
+
+
+}
