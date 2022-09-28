b59c7ecaf87c35862b008fcfa0aa01b26d16934f
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-09-29 01:32:09 +0900

    [Bronze IV] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int n ;
+
+int main(){
+    cin >> n;
+
+    if( n == 0){
+        cout << "YONSEI";
+    }else if(n == 1){
+        cout << "Leading the Way to the Future";
+
+    }
+
+
+
+}
