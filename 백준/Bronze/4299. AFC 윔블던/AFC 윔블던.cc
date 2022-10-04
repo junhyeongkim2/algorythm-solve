243d0791675825cc8e4e12b51bcc3c54dc288d1a
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-10-04 23:54:43 +0900

    [Bronze III] Title: AFC 윔블던, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/4299. AFC 윔블던/AFC 윔블던.cc
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b, c, d;
+	scanf("%d %d", &a, &b);
+
+	a -= b;
+
+	if (a < 0) {
+		printf("-1");
+	}
+	else {
+		if (a % 2 == 0) {
+			c = a / 2 + b;
+			d = a / 2;
+			printf("%d %d", c, d);
+		}
+		else {
+			printf("-1");
+		}
+	}
+}
