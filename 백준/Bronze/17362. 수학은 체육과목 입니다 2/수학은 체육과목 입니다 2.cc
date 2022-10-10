88f5285661beb303ed72f9dc5d1c7f2ec24b3f8e
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-10-11 05:47:56 +0900

    [Bronze IV] Title: 수학은 체육과목 입니다 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17362. 수학은 체육과목 입니다 2/수학은 체육과목 입니다 2.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    if (n <= 5) cout << n << '\n';
+    else
+    {
+        if ((n - 5) / 4 % 2 == 0)
+            cout << 5 - (n-5) % 4 << '\n';
+        else
+            cout << 1 + (n - 5) % 4 << '\n';
+    }
+}
