bd2f1e2ec66bac3f4dc9525654a684c48f0d50c4
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-10-21 02:13:19 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int T;
+string s;
+
+int main(){
+
+    cin >> T;
+
+    while(T--){
+        cin >> s;
+
+        char c1;
+        char c2;
+
+        c1 = s[0];
+        c2 = s[s.length()-1];
+
+        cout << c1 << c2 <<"\n";
+
+
+
+
+
+
+
+    }
+
+
+
+}
