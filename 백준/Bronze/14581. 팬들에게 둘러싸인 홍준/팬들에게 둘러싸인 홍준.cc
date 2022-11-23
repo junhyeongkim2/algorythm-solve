a996b487df670095ef3afc4eb743ee64145c21d8
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-11-23 23:38:54 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+string s;
+
+
+int main() {
+
+    cin >> s;
+
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::"<< s<< "::fan:\n";
+    cout << ":fan::fan::fan:";
+
+
+
+
+
+}
+
+
+
+
