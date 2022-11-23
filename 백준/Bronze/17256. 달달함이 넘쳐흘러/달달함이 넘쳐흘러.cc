be178b86ca1e090cb29158ec1e44c990f1455ac5
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-11-24 03:30:57 +0900

    [Bronze V] Title: 달달함이 넘쳐흘러, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int ax,ay,az;
+int cx,cy,cz;
+
+
+int main() {
+
+    cin >> ax >> ay >> az;
+    cin >> cx >> cy >> cz;
+
+
+
+    cout << abs(cx-az) << " " << cy/ay << " " << abs(cz-ax);
+
+
+
+
+
+
+
+
+}
+
+
+
+
