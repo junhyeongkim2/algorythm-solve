eb8d1a09fd99cf0da6daa28ef24617a22ec9a114
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-11-26 18:50:18 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int ans = 0;
+
+int main() {
+
+
+    for (int i = 0; i <5 ; ++i) {
+        int a ;
+        cin >> a;
+
+        ans += a;
+
+
+    }
+
+    cout << ans <<"\n";
+
+
+
+
+
+
+
+
+}
+
+
+
+
