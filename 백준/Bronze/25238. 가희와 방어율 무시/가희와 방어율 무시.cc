f008011a3534fb7b2387e89f58fc1a963746784c
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-11-27 23:21:42 +0900

    [Bronze V] Title: 가희와 방어율 무시, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25238. 가희와 방어율 무시/가희와 방어율 무시.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+float a, b;
+
+
+int main() {
+
+    cin >> a >> b;
+
+    float c = a * b / 100;
+
+
+    //cout << c;
+    if ( a - c < 100){
+        cout << 1;
+    }else{
+        cout << 0;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
