f7f2aeba03a9d5e64ce083400c47fc59c8e2bc20
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-11-30 05:31:53 +0900

    [Silver V] Title: K-세준수, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1418. K－세준수/K－세준수.cc
+++ 백준/Silver/1418. K－세준수/K－세준수.cc
@@ -1,27 +1,40 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int arr[100001];
 
-int main(){
+int arr[100010];
+
+int main() {
+
     int n,k;
-    int cnt = 0 ;
-    
+    int cnt=0;
     cin >> n >> k;
-    
-    for(int i = 2; i<=n;i++){
-        if(arr[i])continue;
-        for(int j = i; j<=n;j+=i){
-            arr[j] = max(arr[j],i);
+    for (int i = 2; i <=n ; ++i) {
+        if(arr[i]) continue;
+        for (int j = i; j <= n; j+=i) {
+            arr[j] = i;
         }
     }
-    
-    for(int i = 1; i<=n; ++i){
+    for (int i = 1; i <=n ; ++i) {
         if(arr[i]<=k) cnt++;
     }
     cout << cnt <<"\n";
-    
-    
-    
-    
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
