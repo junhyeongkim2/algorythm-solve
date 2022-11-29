0adb894ea600c2238d818708a8d340e6e7f673d7
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-11-29 20:31:32 +0900

    [Bronze V] Title: 세금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n ;
+
+int main() {
+
+    cin >> n;
+
+
+    int a = n * 0.22;
+    int b = n * 0.2 * 0.22;
+
+    cout << n-a <<"\n" << n-b<<"\n";
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
