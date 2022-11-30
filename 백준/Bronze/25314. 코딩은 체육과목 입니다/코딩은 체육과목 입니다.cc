0d642c3faf3755e5c18faff4a3757c11353d5c60
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-12-01 06:09:18 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main() {
+    cin >> n;
+
+
+    int a = n/4;
+
+    for (int i = 0; i < a ; ++i) {
+        cout << "long ";
+    }
+    cout <<"int";
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
