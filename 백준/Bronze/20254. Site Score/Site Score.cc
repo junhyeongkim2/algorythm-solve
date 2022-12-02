00469b6ab864f097f592ca85bbf1c3e275c8ba8b
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-12-03 04:15:32 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a,b,c,d;
+
+
+int main() {
+
+    cin >> a>> b>> c>> d;
+
+    int ans =0;
+
+    ans += a*56;
+    ans += b*24;
+    ans += c*14;
+    ans += d*6;
+
+
+    cout << ans <<"\n";
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
