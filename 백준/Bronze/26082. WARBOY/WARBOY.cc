9873fcce0121e372e3efd1d2d6eff6678557f0c8
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-12-06 15:49:52 +0900

    [Bronze V] Title: WARBOY, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a , b , c ;
+
+
+int main() {
+    cin >> a >> b >> c;
+
+    int ans1= b/a;
+
+
+
+
+    int x= ans1 * 3 * c;
+
+    cout << x <<"\n";
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
