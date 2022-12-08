cf094fb4d6c287607a29146471c91042d3fe03db
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-12-08 11:53:10 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string s;
+
+int main() {
+
+    cout << ".  .   .\n"
+            "|  | _ | _. _ ._ _  _\n"
+            "|/\\|(/.|(_.(_)[ | )(/.";
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
