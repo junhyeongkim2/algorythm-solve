5a61fb3daddb6fe69b9ea5bb071b37c2801bc8ce
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-12-10 22:55:10 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 40 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string s;
+
+int main() {
+
+    int n ;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> s;
+
+        if(s.length()>=6 && s.length()<=9){
+            cout << "yes";
+        }else{
+            cout << "no";
+        }
+        cout << "\n";
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
