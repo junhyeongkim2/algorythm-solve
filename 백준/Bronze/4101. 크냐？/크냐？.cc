53653664dd49b41d0513d06bf0311b882ffac770
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-12-12 02:39:43 +0900

    [Bronze V] Title: 크냐?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a , b;
+
+int main() {
+
+
+    while(true){
+        cin >> a >> b;
+
+        if( a == 0 && b == 0){
+            break;
+        }
+
+        if( a > b){
+            cout << "Yes" << "\n";
+        }else{
+            cout << "No" << "\n";
+        }
+
+
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
