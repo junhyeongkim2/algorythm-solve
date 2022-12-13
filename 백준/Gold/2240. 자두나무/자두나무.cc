42c1f87a5eee2506b21575ddfbe82c8c536c2ad8
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-12-13 23:04:53 +0900

    [Gold V] Title: 자두나무, Time: 0 ms, Memory: 2268 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.cc
+++ 백준/Gold/2240. 자두나무/자두나무.cc
@@ -5,7 +5,7 @@ using namespace std;
 
 int T , W;
 
-int dp[31][2][1001];
+int dp[1001][31][2];
 
 int plum[1001];
 
@@ -17,30 +17,31 @@ int main() {
         cin >> plum[i];
     }
 
-    for (int j = 0; j <= W; ++j) {
-        for (int i = 1; i <=T ; ++i) {
-
-            if(j==0){
-                dp[j][0][i] = dp[j][0][i-1] + (plum[i]==1);
+    for (int i = 0; i <=W ; ++i) {
+        for (int j = 1; j <=T ; ++j) {
+            if(i==0){
+                dp[j][i][0] = dp[j-1][i][0] + (plum[j]==1);
             }else{
-                dp[j][0][i] = max(dp[j][0][i-1] + (plum[i]== 1),dp[j-1][1][i-1] + (plum[i]==1));
-                dp[j][1][i] = max(dp[j][1][i-1] + (plum[i]== 2),dp[j-1][0][i-1] + (plum[i]==1));
-
+                dp[j][i][0] = max(dp[j-1][i][0] + (plum[j]==1) , dp[j-1][i-1][1] + (plum[j]==1));
+                dp[j][i][1] = max(dp[j-1][i][1] + (plum[j]==2) , dp[j-1][i-1][0] + (plum[j]==2));
             }
+
         }
     }
 
     int ans = 0;
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j <= W; j++)
-        {
-            // 마지막에 가능한 모든 이동횟수와, 위치중 가장 큰 값을 출력한다.
-            ans = max(ans, dp[j][i][T]);
+    for (int i = 0; i <2 ; ++i) {
+        for (int j = 0; j <=W ; ++j) {
+            ans = max(dp[T][j][i],ans);
         }
     }
+    cout << ans <<"\n";
 
-    cout << ans << '\n';
+
+
+
+
+
 }
 
 
