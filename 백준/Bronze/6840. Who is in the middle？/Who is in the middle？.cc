65d304fa5185ce12cbf6204707b60562b8809a79
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-12-19 23:49:36 +0900

    [Bronze V] Title: Who is in the middle?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6840. Who is in the middle？/Who is in the middle？.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a, b, c;
+
+vector<int>v1;
+
+int main() {
+
+    for (int i = 0; i <3 ; ++i) {
+
+        int a;
+        cin >> a;
+        v1.push_back(a);
+
+    }
+
+    std::sort(v1.begin(), v1.end());
+    
+
+    cout << v1[1];
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
