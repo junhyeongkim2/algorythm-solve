a5a0ae9c41718260d0a7980f0d70d8eb9794f4b6
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-12-27 00:29:00 +0900

    [Bronze V] Title: Hook, Time: 0 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int dp[1000000];
+
+vector<int>arr;
+
+
+
+int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout << "#  # #### #### #  #\n"
+            "#### #  # #  # # #\n"
+            "#### #  # #  # # #\n"
+            "#  # #### #### #  #";
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
