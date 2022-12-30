2dd912d6887f2e24ef204c5a0cfdd5979e6d81c0
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2022-12-31 01:08:59 +0900

    [Silver I] Title: 정수 삼각형, Time: 12 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,39 +1,66 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 
-int N ;
-
-int dp[501][501];
-int arr[501][501];
-
-
+int tri[500][500];
 
 int main(){
-    cin >> N;
 
-    for(int i = 1 ; i<=N;i++){
-        for(int j = 1 ; j<=i;j++){
-            cin >> arr[i][j];
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+
+    cin >> n;
+
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j <= i; ++j) {
+            cin >> tri[i][j];
+            //cout << tri[i][j];
+        }
+        //cout<<"\n";
+    }
+
+    int ans =0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j <=i ; ++j) {
+            if(j==0){
+                tri[i][j] += tri[i-1][j];
+            }else if(j==i){
+                tri[i][j] += tri[i-1][j-1];
+            }else{
+                tri[i][j] += max(tri[i-1][j-1],tri[i-1][j]);
+            }
+
+            ans = max(tri[i][j],ans);
         }
     }
 
-    dp[1][1] = arr[1][1];
-
-    for (int i = 2; i <=N;i++){
-        for(int j = 1; j <=i;j++){
-            dp[i][j] = (max( dp[i-1][j-1],dp[i-1][j]) + arr[i][j]);
-        }
-    }
-    int max = 0 ;
-    for(int i = 1 ; i<=N;i++){
-        if(max<dp[N][i])max=dp[N][i];
-    }
-    cout << max;
-
-
+    cout << ans <<"\n";
 
 
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
