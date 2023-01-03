62ff4a19d4d7877af36558b427f8b922ea63a38c
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2023-01-03 23:59:08 +0900

    [Bronze V] Title: Correct, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26307. Correct/Correct.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int a ;
+int b;
+
+
+int main(){
+
+    cin >> a;
+    cin >> b;
+
+
+    int c;
+
+    c = a - 9;
+
+    c = c * 60;
+
+    int d;
+
+    cout << c+b<<"\n";
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
