9d4f9307eba189a7b36f80d5e133ac187fe10db2
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2023-01-07 01:45:04 +0900

    [Bronze IV] Title: 치킨댄스를 추는 곰곰이를 본 임스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25191. 치킨댄스를 추는 곰곰이를 본 임스/치킨댄스를 추는 곰곰이를 본 임스.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int n;
+int a, b;
+
+int main(){
+
+
+    cin >> n;
+    cin >> a >> b;
+
+    a/=2;
+
+    int sum  = a+b;
+
+    if(n<sum){
+        cout << n ;
+    }else{
+        cout << sum ;
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
