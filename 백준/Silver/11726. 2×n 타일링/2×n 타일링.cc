1101da2b10151dfb2ca6fd59fd2adadab674738f
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2023-01-07 15:39:22 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -2,44 +2,28 @@
 
 using namespace std;
 
-int n ;
+
+int a;
 int dp[1001];
 
 int main(){
 
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
+    cin >> a;
 
-    cin >> n;
-
-    dp[0] = 0;
     dp[1] = 1;
     dp[2] = 2;
 
     for (int i = 3; i <1001 ; ++i) {
-        dp[i] = (dp[i-1]+dp[i-2])  % 10007;
+        dp[i] = (dp[i-1] + dp[i-2]) % 10007;
     }
 
-    cout << dp[n] <<"\n";
+    cout << dp[a];
+
+
 
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
