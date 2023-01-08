8e5bf5f82f7d892c1ec6a9996e534be7f29312c9
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2023-01-08 22:17:34 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.cc
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -1,20 +1,30 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
-long long  dp[91];
+
+int n;
+long long dp[91];
 
 int main(){
-    int n;
+
     cin >> n;
+
     dp[0]=0;
     dp[1]=1;
-    dp[2]=1;
-    dp[3]=2;
 
-    for(int i = 4; i<=n;i++){
-        dp[i] = (dp[i-1]+dp[i-2]);
+    for (int i = 2; i <=90 ; ++i) {
+        dp[i] = dp[i-1] + dp[i-2];
     }
-    cout << dp[n];
+    cout << dp[n]<<"\n";
+
+
+
 
 
 
 }
+
+
+
+
+
