e2b368f51679733a4f9c2e8ca8b931db8f15436c
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2023-01-11 17:33:41 +0900

    [Gold IV] Title: 사이클 게임, Time: 100 ms, Memory: 9056 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.cc
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int n,m;
+
+int graph[500001];
+
+
+
+int getParent(int x){
+
+    if(x == graph[x]){
+        return x;
+    }
+    return graph[x] = getParent(graph[x]);
+
+}
+
+bool unionParent(int a, int b){
+
+    a = getParent(a);
+    b = getParent(b);
+
+    if(a==b){
+        return true;
+    }else{
+        graph[a] = b;
+        return false;
+    }
+
+}
+
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+
+    for (int i = 0; i <n ; ++i) {
+        graph[i] = i ;
+    }
+
+
+    bool check = false;
+
+    for (int i = 0; i <m ; ++i) {
+        int a, b;
+        cin >> a >> b;
+        if ( unionParent(a,b) == true ){
+            cout << i+1 << "\n";
+            check = true;
+            break;
+        }
+    }
+
+    if(check == false){
+        cout << "0" << "\n";
+    }
+
+
+
+}
+
+
+
+
+
+
