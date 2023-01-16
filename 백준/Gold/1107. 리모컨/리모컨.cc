3361dc427cc3637bd98e33d81d81d102e45212e0
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2023-01-16 19:20:45 +0900

    [Gold V] Title: 리모컨, Time: 212 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1107. 리모컨/리모컨.cc
@@ -0,0 +1,69 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n,m;
+    vector<char>broken_button;
+    cin >> n;
+    cin >> m;
+
+    for (int i = 0; i <m ; ++i) {
+        char a;
+        cin >> a;
+        broken_button.push_back(a);
+    }
+
+
+    vector<int>sizevec;
+
+
+    int minimum= abs(n-100);\
+
+    if(n!=100){
+        for (int i = 0; i <1000001 ; ++i) {
+            bool check = false;
+            if(broken_button.size()!=0){
+                for (int j = 0; j <broken_button.size(); ++j) {
+                    if ( to_string(i).find(broken_button[j]) == string::npos){
+                    }else{
+                        check=true;
+                    }
+                }
+            }
+            if(check==false){
+                int tmp = (abs(n-i)+to_string(i).length());
+                minimum = min(minimum, tmp);
+
+            }
+        }
+        cout << minimum <<"\n";
+    }else{
+        cout << "0"<<"\n";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
