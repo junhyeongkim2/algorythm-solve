d57b631c9d5408859e834195ca0e2d3826ef584a
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2023-01-19 18:33:16 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.cc
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -1,38 +1,35 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
+
+
+vector<int>v;
+
 int main(){
-    int n ;
-    vector<int>v1;
-    cin>>n;
-    for(int i = 0 ; i < n ; i++){
-        int a ;
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i <n ; ++i) {
+        int a;
         cin >> a;
-        v1.push_back(a);
+        v.push_back(a);
     }
-    std::sort(v1.begin(), v1.end());
 
-    int answer=0;
+    std::sort(v.begin(), v.end());
 
-    for(int i = 1 ; i <= n ; i ++){
-        for(int i1=0;i1<i;i1++){
-            answer += v1[i1];
-        }
-        //cout <<answer<<"\n";
+    int sum =0;
+    int ans =0;
 
-
+    for (int i = 0; i <n ; ++i) {
+        ans += v[i];
+        sum += ans;
     }
-    cout << answer;
-
-
-
-
-
-
-
+    cout << sum << "\n";
 
 
 
