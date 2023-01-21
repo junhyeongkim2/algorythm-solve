3dc7a64d5d631d772a7c4597f476057b0d9fe728
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2023-01-21 21:38:34 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.cc
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -1,31 +1,33 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-
-int T;
-int N;
-
-long long dp[101];
-
-
 int main(){
-    cin >> T;
 
-    dp[0]=0;
-    dp[1]=1;
-    dp[2]=1;
-    dp[3]=1;
-    dp[4]=2;
-    for(int i = 5 ; i <=100;i++){
-        dp[i] = dp[i-1] + dp[i-5];
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    int t;
+    cin >> t;
+
+
+    long long int dp[101];
+
+    dp[1] = 1;
+    dp[2] = 1;
+    dp[3] = 1;
+
+    for (int i = 4; i <101 ; ++i) {
+        dp[i] = dp[i-2] + dp[i-3];
     }
-    while(T--){
-        cin >> N;
-        cout << dp[N] << "\n";
+
+    while(t--){
+        int n;
+        cin >> n;
+        cout << dp[n] << "\n";
     }
 
 
 
 }
-
