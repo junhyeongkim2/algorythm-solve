b778c111419afdc5222c9f1db387de16cfe7a675
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2023-01-24 23:46:26 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i <n ; ++i) {
+        int a;
+        cin >> a;
+        cout << a << " " << a << "\n";
+
+
+
+    }
+
+
+
+
+
+
+
+}
