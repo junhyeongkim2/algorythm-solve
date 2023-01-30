9a9ed86d3790f6891bbaf154c6f3123fad8d4df8
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2023-01-31 00:42:21 +0900

    [Gold V] Title: 토마토, Time: 252 ms, Memory: 16280 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,77 +1,89 @@
-#include <iostream>
-#include <queue>
+#import <bits/stdc++.h>
+
 using namespace std;
 
-const int MAX = 1001;
-int M, N;
-int map[MAX][MAX];
-bool visited[MAX][MAX];
-int path[MAX][MAX];
-int dy[] = { 0,0,1,-1 };
-int dx[] = { 1,-1,0,0 };
-queue<pair<int,int>> q;
+int M,N;
+int visited[1001][1001];
+int graph[1001][1001];
+
+queue<pair<pair<int,int>,int>> q;
 
 
+int dx[4] = {1,0,0,-1};
+int dy[4] = {0,-1,1,0};
 
-void BFS() {
-    while (!q.empty()) {
-        int y = q.front().first;
-        int x = q.front().second;
+int bfs(){
+
+    int sum=0;
+    while(!q.empty()){
+        int y  = q.front().first.first;
+        int x  = q.front().first.second;
+        sum = q.front().second;
         q.pop();
-
-        for (int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-
-            if (ny < 0 || nx < 0 || ny >= N || nx >= M)
-                continue;
-            if (map[ny][nx] == 0 && visited[ny][nx] == 0) {
-                visited[ny][nx] = true;
-                q.push(make_pair(ny, nx));
-                path[ny][nx] = path[y][x] + 1;
+        visited[y][x]=1;
+        for(int i = 0 ; i < 4;i++){
+            int ny = y+dy[i];
+            int nx = x+dx[i];
+            if(visited[ny][nx]==0 && nx>=0&&ny>=0&&nx<M&&ny<N&&graph[ny][nx]==0){
+                q.push({{ny,nx},sum+1});
+                graph[ny][nx]=1;
+                visited[ny][nx]=1;
+//                for(int i = 0 ; i < N; i++){
+//                    for(int j = 0 ; j < M ; j++){
+//                        //cout <<  graph[i][j] << " ";
+//                    }
+//                    //cout << "\n";
+//                }
+//                //cout << "\n";
+//                //cout << "---------------------"<<"\n";
             }
         }
     }
-
+    return sum;
 }
 
-int main() {
-    cin >> M >> N;
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            cin >> map[i][j];
+int main(void){
+
+    cin >> M  >> N;
+
+    for(int i = 0 ; i < N; i++){
+        for(int j = 0 ; j < M ; j++){
+            cin >> graph[i][j];
         }
     }
 
-    /*익은 토마토를 찾아 BFS의 출발지로 queue에 삽입*/
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (map[i][j] == 1) {
-                q.push(make_pair(i, j));
-            }
-        }
-    }
-    BFS();
-
-
-    /*익지 않은 토마토 존재하면 -1 출력*/
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (map[i][j] == 0 && path[i][j]==0) { //익지 않음 토마토가 있으나 방문한적 없음
-                cout << -1;
-                return 0;
+    for(int i = 0 ; i < N; i++){
+        for(int j = 0 ; j < M ; j++){
+            if(graph[i][j]==1){
+                q.push({{i,j},0});
             }
         }
     }
 
-    /*방문 일자 저장 배열 중 최대값 출력*/
-    int ans = -1;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            ans = max(ans,path[i][j]);
+
+    int ans = bfs();
+
+    bool check = false;
+    for(int i = 0 ; i < N; i++){
+        for(int j = 0 ; j < M ; j++){
+            if(graph[i][j]==0){
+                check = true;
+            }
         }
     }
-    cout << ans;
+
+    if(check==true){
+        cout << -1;
+    }else{
+        cout << ans;
+    }
+
+
+
+
+
+
+
 
 }
