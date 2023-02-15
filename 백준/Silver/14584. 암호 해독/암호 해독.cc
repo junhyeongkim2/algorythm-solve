f520659afa4c71acbf862efdf6aff5298ca0565e
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2023-02-16 01:00:11 +0900

    [Silver IV] Title: 암호 해독, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/14584. 암호 해독/암호 해독.cc
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+string s;
+int n;
+vector<string>v;
+
+int main() {
+
+    cin >> s;
+    cin >> n;
+
+    for(int i = 0; i < n ; i++){
+        string a;
+        cin >> a;
+        v.push_back(a);
+    }
+
+    for(int j = 0 ; j < 25; j ++){
+        string temp;
+        for(int i = 0 ; i < s.size(); i++){
+            if ( s[i] + j >  122  ){
+                int a = (s[i] + j) - 122;
+                temp += char(96 + a);
+            }else{
+                temp += (s[i] + j);
+            }
+        }
+
+        for(int i = 0; i < v.size(); i++){
+            if ( temp.find(v[i]) != string::npos ){
+                cout << temp <<"\n";
+                break;
+            }
+        }
+    }
+
+
+}
