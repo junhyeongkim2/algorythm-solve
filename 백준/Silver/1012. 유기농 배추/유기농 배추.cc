bb3b018b912c187c11f694ed235c89d114bdf871
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2023-02-18 18:55:58 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,66 +1,78 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 51;
-int T,M,N,K;
-int map[MAX][MAX];
-int visited[MAX][MAX];
-int dy[] = {0,0,-1,1};
-int dx[] = {-1,1,0,0};
 
-void reset() {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            map[i][j] = 0;
+int T;
+int n;
+int m;
+int k;
+int graph[51][51];
+int visited[51][51];
+
+int dx[4] = {0,0,-1,1};
+int dy[4] = {1,-1,0,0};
+
+void dfs(int y, int x){
+
+    visited[y][x] = 1;
+
+    for(int i = 0 ; i < 4 ; i ++){
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+
+        if(visited[ny][nx] == 0 && ny>=0 && nx>=0 && ny < n && nx < m && graph[ny][nx] == 1){
+            dfs(ny,nx);
         }
+
+
+
     }
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            visited[i][j] = 0;
-        }
-    }
-}
 
-void DFS(int y, int x) {
-    visited[y][x]=1;
-    for (int i = 0; i < 4; i++) {
-        int nx= x + dx[i];
-        int ny= y + dy[i];
-        if (nx < 0 || nx>=M || ny<0 || ny >=N)
-            continue;
-        if(map[ny][nx]==1&&visited[ny][nx]==0){
-            DFS(ny,nx);
-        }
-    }
+
+
 }
 
 
 int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     cin >> T;
 
     while(T--){
-        cin >> M >> N >> K;
+        cin >> n >> m >> k;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                graph[i][j] = 0;
+            }
+        }
+        for(int i = 0 ; i < k; i++){
+            int a, b;
+            cin >> a >> b;
+            graph[a][b] = 1;
+        }
 
-        reset();
 
-         while(K--){
-            int x,y;
-            cin >> x >> y;
-            map[y][x] = 1;
-          }
-
-
-        int count =0;
-
-        for(int i=0;i<N;i++){
-            for(int j=0;j<M;j++){
-                if(map[i][j] == 1 && visited[i][j]==0){
-                    DFS(i,j);
-                    count++;
+        int ans  = 0 ;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(graph[i][j] == 1 && visited[i][j] == 0){
+                    dfs(i,j);
+                    ans ++;
                 }
             }
         }
-        cout << count << endl;
+
+        cout << ans << "\n";
+        memset(visited,0,sizeof visited);
+        memset(graph,0,sizeof graph);
     }
+
+
+
+
+
 }
