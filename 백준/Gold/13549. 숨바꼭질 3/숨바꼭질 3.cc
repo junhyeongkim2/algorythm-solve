322561367371174f2f1f2f77b72fc76d6d640afb
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2023-03-03 01:48:38 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2544 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int n,k;
+
+int p[100001]={};
+int min_w= 1000000;
+
+void shortest(){
+
+    queue<int>q;
+    q.push(n);
+    p[n]=0;
+
+    while(!q.empty()){
+
+        int cur = q.front();
+        q.pop();
+
+        if(cur == k){
+            min_w = p[cur];
+            return;
+        }
+
+        if( cur * 2 < 100001 && p[2 * cur] > p[cur]){
+            p[cur * 2 ] = p[cur];
+            q.push(cur*2);
+        }
+        if(cur + 1 < 100001 && p[1 + cur] > p[cur]+1){
+            p[cur + 1 ] = p[cur]+1;
+            q.push(cur+1);
+        }
+        if( cur - 1 >= 0 && p[cur - 1] > p[cur]+1){
+            p[cur - 1] = p[cur]+1;
+            q.push(cur - 1);
+        }
+    }
+
+
+
+}
+
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> k;
+
+
+    for(int i = 0 ; i < 100001; i++){
+        p[i] = 1000000;
+    }
+
+    shortest();
+
+    cout << min_w << "\n";
+    
+    return 0;
+
+}
+
