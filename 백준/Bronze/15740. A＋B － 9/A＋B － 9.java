51a8e7634a63491d498ac785b830f1280405bc73
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2023-04-17 23:58:56 +0900

    [Bronze V] Title: A+B - 9, Time: 428 ms, Memory: 20640 KB -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -0,0 +1,18 @@
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        BigInteger a,b;
+
+        a = sc.nextBigInteger();
+        b = sc.nextBigInteger();
+
+        System.out.println((a.add(b)).toString());
+
+    }
+}
