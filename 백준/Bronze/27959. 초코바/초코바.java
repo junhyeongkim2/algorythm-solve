adf7ac217fd16106f52784b230e1911deb0bb860
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2023-04-20 23:40:32 +0900

    [Bronze V] Title: 초코바, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int n,m;
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        n*=100;
+
+        if(n<m){
+            System.out.println("No");
+        }else{
+            System.out.println("Yes");
+        }
+
+
+
+
+
+
+
+
+    }
+}
