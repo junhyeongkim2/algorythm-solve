fa86397e41bdeab9dc4404e8cca103d63658e549
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2023-04-20 23:37:54 +0900

    [Bronze V] Title: 초코바, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.cc
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n,m;
+
+    cin >> n >> m;
+
+    n *= 100;
+
+    if(n < m){
+        cout << "No" << "\n";
+    }else{
+        cout << "Yes" << "\n";
+    }
+
+
+
+
+
+
+
+
+}
+
