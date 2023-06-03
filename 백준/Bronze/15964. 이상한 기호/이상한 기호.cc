c7706d95c5d45ad4549646284381b1cb4d7d7d2b
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2023-06-03 23:37:42 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int N,K;
+vector<string>ans;
+
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int a,b;
+    cin >> a >> b;
+
+    cout << (a+b) * (a-b);
+
+
+
+
+
+
+
+    return 0;
+}
