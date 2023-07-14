a0696a6e36bf31637a539bc027a207e4482c59cc
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2023-07-14 21:23:22 +0900

    [Bronze V] Title: 身長 (Height), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+
+
+    int a,b;
+    cin >> a>>b;
+    cout << b-a<<"\n";
+
+
+    return 0;
+}
