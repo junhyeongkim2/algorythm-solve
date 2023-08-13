8a9ceca5403c3b350c33fab543aca0d15d30ef90
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2023-08-13 22:12:51 +0900

    [Bronze V] Title: HI-ARC=?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cout.tie(0);
+    cin.tie(0);
+
+
+    int h,i,a,r,c;
+
+    cin >> h >> i >> a >> r >> c;
+
+    cout << h*i - a*r*c;
+
+
+
+
+
+
+
+
+
+}
+
