393316c08d607abd303705fbb6858f6c3695c4a2
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2023-09-03 23:02:31 +0900

    [Bronze IV] Title: 줄번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    cin.ignore();
+
+    for(int i = 0 ; i < n; i++){
+        string s;
+        getline(cin,s);
+
+        cout << i+1 << ". " << s << "\n";
+
+
+
+
+    }
+
+
+
+
+
+    return 0;
+}
