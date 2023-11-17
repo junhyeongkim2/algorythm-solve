0f93ce28ae8e0c97435401c25bf6b9296ba9c5e1
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2023-11-17 22:43:06 +0900

    [Gold V] Title: 호석이 두 마리 치킨, Time: 308 ms, Memory: 23388 KB, Score: 19 point -BaekjoonHub

--- 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.java
+++ 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.java
@@ -1,81 +1,88 @@
 import java.io.*;
-import java.sql.Array;
 import java.util.*;
-import java.util.stream.Collectors;
-
 public class Main {
-
-    private static int INF = 101;
-    private static int[][] graph = new int[101][101];
-
-    public static void main(String[] args) throws NumberFormatException, IOException {
-
+    static int[][] floyd;
+    static int N, M;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Scanner sc = new Scanner(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st1.nextToken());
+        M = Integer.parseInt(st1.nextToken());
 
-        int n, m;
-        n = sc.nextInt();
-        m = sc.nextInt();
+        floyd = new int[N][N];
+        
+        for(int i = 0 ; i < N ; i++) {
+            for(int j = 0 ; j < N ; j++) {
+                floyd[i][j] = 101; //점의 최대 개수는 100개이므로, 점과 점은 아무리 멀어도 거리 99가 최대임.
+                //즉, 거리 101로 해주는 것은 다익스트라의 Integer.MAX_VALUE와 같음.
+            }
+        }
+        //존재하는 길이 없다. 를 101 로 표현.
+        //있으면, 거리는 전부 1이니 해당 값 넣기
+        
+        for(int i = 0 ; i < M ; i++) {
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st2.nextToken());
+            int b = Integer.parseInt(st2.nextToken());
+            floyd[a-1][b-1] = 1;
+            floyd[b-1][a-1] = 1;
+        }
+        //초깃값.
 
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                graph[i][j] = INF;
-                if (i == j) {
-                    graph[i][j] = 0;
+        for(int i = 0 ; i < N ; i++) {
+            for(int j = 0 ; j < N ; j++) {
+                if(i == j) continue;
+                for(int k = 0 ; k < N ; k++) {
+                    if(i == k || j == k) continue;
+                    
+                    if(floyd[j][k] > floyd[j][i] + floyd[i][k]) floyd[j][k] = floyd[j][i] + floyd[i][k];
+                    
                 }
             }
         }
+        
+        //플로이드 워셜로 각 점에서 각 점까지의 거리의 최솟값 다 채운 후
+        //플로이드 워셜 로직 = a->c로 가는 값보다 b를 거쳐 a->b + b->c 값이 더 작다면 작은 값을 택하는 알고리즘.
+        
+        int min = Integer.MAX_VALUE;
+        int n1 = 0;
+        int n2 = 0;
 
-        for (int i = 0; i < m; i++) {
-            int a, b;
-            a = sc.nextInt();
-            b = sc.nextInt();
-            graph[a][b] = 1;
-            graph[b][a] = 1;
-        }
+        for(int i = 0 ; i < N ; i++) {
+            for(int j = 0 ; j < N ; j++) {
+                if(i == j) continue;
+                //두 점을 고른다. (1, 2부터 N-1, N까지)
 
-        for (int k = 1; k <= n; k++) {
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    if (graph[i][k] + graph[k][j] < graph[i][j]) {
-                        graph[i][j] = graph[i][k] + graph[k][j];
-                    }
+                int now = sum(i, j);
+                //고른 두 점을 기준으로 각각의 점의 거리의 최솟값을 더해나간다.
+                //1, 2를 골랐으면
+                //sum(1, 2) = 그 둘(1, 2)에서 3까지의 거리의 최솟값 + ... + 그 둘(1, 2)에서 N 까지의 거리의 최솟값
+
+                if(min > now) {
+                    n1 = i+1;
+                    n2 = j+1;
+                    min = now;
                 }
-            }
-        }
-
-        int min = 101;
-
-        int p1=0;
-        int p2=0;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if(i==j) continue;
-                int now = sum(i,j,n);
-                if(min > now){
-                    p1= i;
-                    p2 = j;
-                    min = now ;
-                }
+                //해당 값이 최솟값이 된다면 두 점을 기록해놓는다.
 
             }
         }
 
-        System.out.println(p1+" "+p2+" "+min*2);
+        //기록한 두 점과 최솟값을 출력한다.
+        bw.write(String.valueOf(n1) + " " + String.valueOf(n2) + " " + String.valueOf(min*2)); //min*2는 왕복이라서.
+        bw.flush();
+        bw.close();
 
 
     }
 
-
-
-
-    public static int sum(int p1, int p2, int n) {
-        int sum =0;
-        for(int i = 1 ; i <= n ; i ++){
-            if(i==p1||i==p2)continue;
-            sum += Math.min(graph[p1][i],graph[p2][i]);
+    static int sum(int n1, int n2) {
+        int sum = 0;
+        for(int i = 0 ; i < N ; i++) {
+            if(i == n1 || i == n2) continue;
+            sum += Math.min(floyd[n1][i], floyd[n2][i]);
         }
 
         return sum;
