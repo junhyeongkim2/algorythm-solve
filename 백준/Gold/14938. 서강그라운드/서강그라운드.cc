6cccd893705a643f76fe7a136ab1bbb50c763cf5
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2023-11-24 19:16:58 +0900

    [Gold IV] Title: 서강그라운드, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.cc
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.cc
@@ -4,55 +4,72 @@ using namespace std;
 
 #define INF 1000000000
 
-int ans, cnt;
-int n, m, r, a, b, l;
-int local[101];
-int dist[101][101];
+int n, m, r;
+int items[101];
+int graph[101][101];
 
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+
+void setting() {
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i != j) {
+                graph[i][j] = INF;
+            }
+        }
+    }
+}
+
+void floyd() {
+
+
+    for (int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (graph[i][j] > graph[i][k] + graph[k][j]) {
+                    graph[i][j] = graph[i][k] + graph[k][j];
+                }
+            }
+        }
+    }
+
+}
+
+int find() {
+
+    int maxSum = 0;
+
+    for (int i = 1; i <= n; i++) {
+        int sum = items[i];
+        for (int j = 1; j <= n; j++) {
+            if (i != j && graph[i][j] <= m) {
+                sum += items[j];
+            }
+        }
+        maxSum = max(maxSum, sum);
+    }
+    return maxSum;
+}
+
+
+int main() {
 
     cin >> n >> m >> r;
 
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++)
-        {
-            if (i != j) dist[i][j] = INF;
-        }
-
-    // 지역별 아이템 갯수 저장
-    for (int i = 1; i <= n; i++) cin >> local[i];
-
-    // 길과 길 사이의 거리 저장
-    for (int i = 0; i < r; i++)
-    {
+    setting();
+    for (int i = 1; i <= n; i++) {
+        cin >> items[i];
+    }
+    for (int i = 0; i < r; i++) {
+        int a, b, l;
         cin >> a >> b >> l;
-        dist[a][b] = l;
-        dist[b][a] = l;
+        graph[a][b] = l;
+        graph[b][a] = l;
     }
 
-    // 플루이드 와샬
-    for (int k = 1; k <= n; k++)
-        for (int i = 1; i <= n; i++)
-            for (int j = 1; j <= n; j++)
-            {
-                if (dist[i][k] + dist[k][j] < dist[i][j])
-                    dist[i][j] = dist[i][k] + dist[k][j];
-            }
 
-    for (int i = 1; i <= n; i++)
-    {
-        cnt = local[i]; // 시작지점 아이템 파밍
-        for (int j = 0; j <= n; j++)
-        {
-            // 수색가능 범위 아이템 파밍
-            if (i != j && dist[i][j] <= m) cnt += local[j];
-        }
-        ans = max(ans, cnt);
-    }
+    floyd();
+    cout << find();
 
-    cout << ans;
 
 }
