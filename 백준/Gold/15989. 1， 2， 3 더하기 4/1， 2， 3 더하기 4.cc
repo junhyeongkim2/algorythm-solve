ebdb91a302ec41bab33274b1e156c97f44ec1bc7
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2024-01-21 16:20:31 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 24 ms, Memory: 392724 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int dp[10001][10001];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    dp[0][1] = 1;
+    dp[1][1] = 1;
+
+    for (int i = 2; i <= 10000; i++) {
+        dp[i][1] = dp[i - 1][1];
+        dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
+        if (i >= 3) {
+            dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
+        }
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        cout << dp[a][1] + dp[a][2] + dp[a][3] << "\n";
+    }
+
+
+}
+
+
