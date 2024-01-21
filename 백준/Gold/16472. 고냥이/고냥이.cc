1239ccf8f00d3f06b00a18f20d0697b3c7516d2a
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2024-01-21 23:37:59 +0900

    [Gold IV] Title: 고냥이, Time: 4 ms, Memory: 2576 KB -BaekjoonHub

---
+++ 백준/Gold/16472. 고냥이/고냥이.cc
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+#define visit trash
+using namespace std;
+
+int n;
+string s;
+
+map<char, int> m;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    cin >> s;
+
+    int left = 0;
+    int right = 0;
+    string tempS = "";
+    int visitCount = 0;
+    int ans = 0;
+
+
+    while (left <= right && right < s.length()) {
+
+        if (visitCount <= n) {
+            tempS += s[right];
+
+
+            if (m[s[right]] == 0) {
+                visitCount++;
+            }
+
+            if (visitCount <= n) {
+                int tempSLength = tempS.length();
+                ans = max(tempSLength, ans);
+            }
+
+            m[s[right]]++;
+            right++;
+
+
+//            cout << tempS << " / 1 " << visitCount << "\n";
+
+        } else if (visitCount > 2) {
+            m[s[left]]--;
+            if (m[s[left]] == 0) {
+                visitCount--;
+            }
+            tempS.erase(0, 1);
+            left++;
+//            cout << tempS << " / 2 " << visitCount << "\n";
+
+        }
+
+
+    }
+
+    cout << ans << "\n";
+
+
+}
+
