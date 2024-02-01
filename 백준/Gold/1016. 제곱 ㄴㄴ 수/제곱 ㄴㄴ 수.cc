6cbda082fae64f5de6baa87c28fa5889dc1201df
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2024-02-01 23:49:35 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 20 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long num[1000001];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long min, max;
+    cin >> min >> max;
+
+    for (long long i = min; i <= max; i++) {
+        num[i - min] = i;
+    }
+
+    for (long long i = 2; i * i <= max; i++) {
+        long long N = min / (i * i);
+
+        if (min % (i * i) != 0)
+            N++;
+
+        while (N * i * i <= max) {
+            num[N * i * i - min] = 0;
+            N++;
+        }
+    }
+
+    int ans = 0;
+    for (long long i = min; i <= max; i++) {
+        if (num[i - min] != 0) {
+            ans++;
+        }
+    }
+    cout << ans << "\n";
+
+
+}
