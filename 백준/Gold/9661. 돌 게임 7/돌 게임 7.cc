44382917dddff410b30e9f48f668ca5a0238dac7
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2024-02-02 09:13:09 +0900

    [Gold II] Title: 돌 게임 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9661. 돌 게임 7/돌 게임 7.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long int n;
+    cin >> n;
+
+    if( n % 5 == 0 || n % 5 == 2){ 
+        cout << "CY";
+    }else{
+        cout << "SK";
+    }
+
+
+
+}
