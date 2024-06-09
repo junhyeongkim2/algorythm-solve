d07a04fbdc373343bfa0b1f9a8ca5209f1f19184
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2024-06-09 17:50:06 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,27 +1,20 @@
 class Solution {
     public int solution(long num) {
         
-          int answer = 0;
-        long nm = num;
-        if(nm == 1){
-            return 0;
-        }
-        while(true){
-            if(nm % 2 == 0){
-                nm = nm / 2;
-                answer = answer + 1; 
-            }else if(nm % 2 != 0){
-                nm = nm * 3 + 1;
-                answer = answer + 1;
-            }
-            if(nm == 1){
-                break;
-                }else if(answer == 500){
-                    answer = -1;
-                    break;
-                }
-        }
-        return answer;
+      	int answer = 0;
+
+		while(num > 1)
+		{
+			if(num%2 == 0) num /= 2; 
+			else num = num * 3 + 1; 
+			answer++;
+      
+			if(answer > 500) {
+				return -1;
+			}
+		}
+
+		return answer;
         
     }
 }
