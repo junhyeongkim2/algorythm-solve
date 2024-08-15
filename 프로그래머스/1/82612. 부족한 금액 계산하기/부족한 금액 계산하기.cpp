095a370500850602abf49dc640a8f0ba488198fd
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2024-08-15 20:50:18 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long solution(int price, int money, int count)
+{
+    long long answer = 0;
+    
+    for(int i = 1 ; i <= count ; i++){
+        answer += price * i;
+    }
+            
+    
+    if(money - answer <= 0) {
+        return answer - money;
+    }
+    
+
+    return 0;
+}
