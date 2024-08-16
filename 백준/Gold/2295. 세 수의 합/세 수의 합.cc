2aac524c2ee69f2354139d83faaec7c9b44946fc
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2024-08-17 00:31:56 +0900

    [Gold IV] Title: 세 수의 합, Time: 596 ms, Memory: 33188 KB -BaekjoonHub

---
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll arr[1234] = {0};
+map<ll,int> m;
+
+
+int main(){
+	int N;
+	cin >> N;
+	for(int i=0; i<N; i++) cin >> arr[i];
+	
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			m[arr[i]+arr[j]] = 1;
+		}
+	}
+	
+	ll ans = 0;
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			if(m.find(arr[i]-arr[j]) != m.end()){
+				ans = max(ans, arr[i]);
+			}
+		}
+	}
+	cout << ans;
+	return 0;
+}
