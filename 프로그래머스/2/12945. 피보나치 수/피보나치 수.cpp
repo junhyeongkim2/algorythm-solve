00647a7967469ffa146bba0a9456cb55592c337a
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2024-08-16 15:41:13 +0900

    [level 2] Title: 피보나치 수, Time: 0.54 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int dp[100001];
+
+int solution(int n) {
+    
+    
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 1;
+    
+    for(int i = 3 ; i <= n; i++ ){
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+    }
+    
+    int answer = dp[n];
+    return answer;
+}
