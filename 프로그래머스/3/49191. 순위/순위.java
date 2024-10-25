b8683e5ceceb3e79bc9bb5db06c49cdd27b08b6b
kimjunhyeong <52617204+junhyeongkim2@users.noreply.github.com>
2024-10-26 00:39:56 +0900

    [level 3] Title: 순위, Time: 59.94 ms, Memory: 92.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -0,0 +1,63 @@
+class Solution {
+    
+    final int INF = 99999999;
+    int[][] dist;
+    int n;
+    
+    public int solution(int n, int[][] results) {
+        
+        dist = new int [n+1][n+1];
+    
+        
+        for(int i = 0 ; i < results.length; i++){
+            dist[results[i][0]][results[i][1]] = 1;
+            dist[results[i][1]][results[i][0]] = -1;
+        }
+        
+        
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                                        
+                    if(dist[k][j] == 1 && dist[j][i] == 1){
+                        dist[k][i] = 1;
+                        dist[i][k] = -1;
+                    }
+                    
+                    if(dist[k][j] == -1 && dist[j][i] == -1){
+                        dist[k][i] = -1;
+                        dist[i][k] = 1;
+                    }
+                    
+                    
+                }
+            }
+        }
+        
+        int answer = 0;
+        
+        
+        
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= n ; j++){
+                System.out.print(dist[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        for(int i = 1 ; i <= n ; i++){
+            int cnt = 0;
+            for(int j = 1 ; j <= n ; j++){
+                if(dist[i][j] != 0){
+                    cnt++;
+                }
+            }
+            if(cnt == n - 1){
+                answer++;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
